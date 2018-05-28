4891cf85171de1eaaf0a39c053c5957e0f331cfd
Moises Dobarganes <designerexpert@gmail.com>
2018-05-28 14:23:14 -0400

    First Feature Achieved Styled the notes list component Hard Coded some notes in HTML CSS finished for Feature 1

--- lambda-notes/src/components/MainContent/MainContent.js
+++ lambda-notes/src/components/MainContent/MainContent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './index.css';
+import NotesList from '../NotesList/NotesList'
 
 class MainContent extends Component {
     render() {
         return (
             <div className='main_container'>
-                MAIN CONTENT PLACEHOLDER
+                <NotesList/>
             </div>
         );
     }
--- lambda-notes/src/components/NotesList/NotesList.js
+++ lambda-notes/src/components/NotesList/NotesList.js
@@ -4,8 +4,52 @@ import './index.css';
 class NotesList extends Component {
     render() {
         return (
-            <div>
-                NOTES LIST PLACEHOLDER
+            <div className='notesList_container'>
+                <div><h3 className='content_header'>Your Notes:</h3></div>
+                <div className='notesList'>
+                    <div className='note'>
+                        <div>
+                            <h4>Note Title</h4>
+                            <hr></hr>
+                            <p>Lorem ipsum</p>
+                        </div>
+                    </div>
+                    <div className='note'>
+                        <div>
+                            <h4>Note Title</h4>
+                            <hr></hr>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                        </div>
+                    </div>
+                    <div className='note'>
+                        <div>
+                            <h4>Note Title</h4>
+                            <hr></hr>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                        </div>
+                    </div>
+                    <div className='note'>
+                        <div>
+                            <h4>Note Title</h4>
+                            <hr></hr>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                        </div>
+                    </div>
+                    <div className='note'>
+                        <div>
+                            <h4>Note Title</h4>
+                            <hr></hr>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                        </div>
+                    </div>
+                    <div className='note'>
+                        <div>
+                            <h4>Note Title</h4>
+                            <hr></hr>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
