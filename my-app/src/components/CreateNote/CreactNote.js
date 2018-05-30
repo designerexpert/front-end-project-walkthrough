66b392baec60a9120cb7cafc4fa3e54b3cc1ab10
Dil Sim <dilyasom@gmail.com>
2018-05-30 15:56:31 -0700

    working on CreateNote page

--- my-app/src/components/CreateNote/CreactNote.js
+++ my-app/src/components/CreateNote/CreactNote.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './index.css';
 
 class CreactNote extends Component {
-    render() {
-        return (
-            <div>
-                CN placeholder
-            </div>
+        render() {
+            return (
+                <div className='noteView_container'>
+                    <div className='noteView_topContent'>
+                        <div className='content_header'>
+                            Creact New Note;
+                        </div>
+                    </div>
+                    <div  className='notes_Form'>
+                            <input type='text' className='createNoteTitle' placeholder='Note Title' />
+                            <textarea className='createNoteBody' placeholder='Note Content' rows="20" />
+                            <button className='createNote_button'>Save</button>
+                    </div>
+                </div>
         );
     }
 }
--- my-app/src/components/MainContent/MainContent.js
+++ my-app/src/components/MainContent/MainContent.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './index.css';
 import NoteList from '../NoteList/NoteList';
 import NoteView from '../NoteView/NoteView';
+import CreateNote from '../CreateNote/CreactNote';
 
 class MainContent extends Component {
     render() {
         return (
             <div className='main_container'>
-                <NoteView />
+                <CreateNote />
             </div>
         );
     }
