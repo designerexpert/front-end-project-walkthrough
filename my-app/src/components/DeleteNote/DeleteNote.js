1292fead2542879fb70d44782769e5ee4fbfa7fe
Dil Sim <dilyasom@gmail.com>
2018-05-31 16:23:58 -0700

    delete function works

--- my-app/src/components/DeleteNote/DeleteNote.js
+++ my-app/src/components/DeleteNote/DeleteNote.js
@@ -17,11 +17,11 @@ class DeleteNote extends Component {
             // <div className='delete_wrapper hidden'>
             //if this.props.toggle is true the modal is shown, if not the class 'hidden' is added which is = display: none;
             <div  className={toggleInModal ? 'delete_wrapper' : 'hidden'}>
-                <div><h4>Are you sure...</h4>
+                <div><h4>Are you sure you want to delete this?</h4>
                 </div>
                 <div className='modalButtons'>
                     <div className='createNote_button danger' 
-                         onClick={() => {this.handleDeleteNote}}
+                         onClick={this.handleDeleteNote}
                     >Delete</div>
                     <div className='createNote_button' 
                         onClick={this.props.hideModal}
